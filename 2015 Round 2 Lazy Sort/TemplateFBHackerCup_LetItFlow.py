8b6dd54f0e3d0aec9217414ba054dde887336efa
Gerrit Schoettler <gerrit.schoettler@tuhh.de>
2018-07-31 21:41:00 +0200

    Rewrote 2018 Let it Flow, Started 2015 LazySort
    Practice Day1 for Round 2

---
+++ 2015 Round 2 Lazy Sort/TemplateFBHackerCup_LetItFlow.py
@@ -0,0 +1,25 @@
+f = open("let_it_flow_sample_input.txt",'r')
+output=open("let_it_flow_submission.txt",'w')
+input = f.readline
+
+for cnum in range(1,int(input())+1):
+    n = int(input())
+    line1 = input().strip()
+    line2 = input().strip()
+    line3 = input().strip()
+    if n%2==1 or line1[0]=='#' or line3[-1]=='#' or ('#' in line2):
+        ans = 0
+    else:
+        ans = 1
+        for i in range(1,n-1,2):
+            t = 0
+            if line1[i] == '.' and line1[i + 1] == '.': t += 1
+            if line3[i] == '.' and line3[i + 1] == '.': t += 1
+            ans = ans * t
+    ans = ans % 1000000007
+    print("Case #%d: %d"%(cnum,ans),file=output)
+
+f.close()
+output.close()
+print('Input file closed:',f.closed)
+print('Output file closed:',output.closed)
