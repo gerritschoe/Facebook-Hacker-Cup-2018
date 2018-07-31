fc677b8afc3c7b69d18a0ed8ef5c3c231006836c
Gerrit Schoettler <gerrit.schoettler@tuhh.de>
2018-08-01 01:44:34 +0200

    finished lazy sort
    Lazy sort was Problem 1 in 2015 Round 2

--- 2015 Round 2 Lazy Sort/lazy_sort.py
+++ 2015 Round 2 Lazy Sort/lazy_sort.py
@@ -1,24 +1,45 @@
+# Written by Gerrit Schoettler, 2018
+
 f = open("lazy_sort_sample_input.txt", 'r')
 output = open("lazy_sort_sample_submissionTest.txt", 'w')
+#f = open("lazy_sort.txt", 'r')
+#output = open("lazy_sort_submission.txt", 'w')
 input = f.readline
 
 for cnum in range(1, int(input()) + 1):
     n = int(input())
-    line1 = input().strip()
-    line2 = input().strip()
-    line3 = input().strip()
-    if n % 2 == 1 or line1[0] == '#' or line3[-1] == '#' or ('#' in line2):
-        ans = 0
-    else:
-        ans = 1
-        for i in range(1, n - 1, 2):
-            t = 0
-            if line1[i] == '.' and line1[i + 1] == '.': t += 1
-            if line3[i] == '.' and line3[i + 1] == '.': t += 1
-            ans = ans * t
+    aOriginal = input().strip().split()
+    ans = 'no'
+    answer = [1, 1]
+    for side in range(2):
+        b = []
+        a = []
+        a = aOriginal * 1
+        b.append(int(a.pop(-side)))
+        A = b[0]
+        B = b[0]
 
-    ans = ans % 1000000007
-    print("Case #%d: %d" % (cnum, ans), file=output)
+        for i in range(n-1):
+            print('i = ', i,'a = ', a,'A = ', A, 'B = ', B)
+            if int(a[0]) == A + 1:
+                A = A +1
+                a.pop(0)
+            elif int(a[-1]) == A + 1:
+                A = A+1
+                a.pop(-1)
+            elif int(a[0]) == B-1:
+                B=B-1
+                a.pop(0)
+            elif int(a[-1])==B-1:
+                B=B-1
+                a.pop(-1)
+            else:
+                answer[side] = 0
+                break
+    if 1 in answer:
+        ans = 'yes'
+
+    print("Case #%d: %s" % (cnum, ans), file=output)
 
 f.close()
 output.close()
