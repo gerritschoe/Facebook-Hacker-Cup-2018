cbbfbabb9dc3a40a24a9f99fabe4b870cb43dfe9
Gerrit Schoettler <gerrit.schoettler@tuhh.de>
2018-08-04 20:10:52 +0200

    save problem 1 round 2
    round 2

--- Round 2 Problem 1/ethan_finds_the_shortest_path.py
+++ Round 2 Problem 1/ethan_finds_the_shortest_path.py
@@ -8,27 +8,79 @@ for cnum in range(1, int(input()) + 1):
     N, K = input().strip().split()
     N = int(N)
     K = int(K)
-
-    print(N, K)
+    graph = []
+    E = 0
+    #print(N, K)
     diff = 0
 
     if K >= 3 and N >= 3:
         if K <= N:
             diff = (K-1)*K/2 - K
+
         elif K > N:
             KminN = K - N + 1
             diff = (K - 1) * K / 2 - K - (KminN - 1) * KminN / 2
+        E = min(N, K)
 
     elif N == 2:
         diff = 0
+        E = 1
+        #graph = [1, 2, K]
     elif K == 2:
         diff = 0
+        E = 1
+
     elif K == 1:
         diff = 0
+        E = 1
 
     ans = diff
     print("Case #%d: %d" % (cnum, ans))
     print("Case #%d: %d" % (cnum, ans), file=output)
+
+    print(E, file=output)
+
+    if K >= 3 and N >= 3:
+        Uj = 1
+        Vj = N
+        Wj = K
+        line = [Uj, Vj, Wj]
+        print(*line, sep=' ', file=output)
+        for j in range(1, min(K, N)-1):
+            Uj = j
+            Vj = j + 1
+            Wj = K - j
+            line = [Uj, Vj, Wj]
+            print(*line, sep=' ', file=output)
+
+        Uj = min(K, N) - 1
+        Vj = N
+        Wj = K - min(K, N) + 1
+        line = [Uj, Vj, Wj]
+        print(*line, sep=' ', file=output)
+
+    elif N == 2:
+        Uj = 1
+        Vj = N
+        Wj = K
+        line = [Uj, Vj, Wj]
+        print(*line, sep=' ', file=output)
+
+    elif K == 2:
+        Uj = 1
+        Vj = N
+        Wj = K
+        line = [Uj, Vj, Wj]
+        print(*line, sep=' ', file=output)
+
+    elif K == 1:
+        Uj = 1
+        Vj = N
+        Wj = K
+        line = [Uj, Vj, Wj]
+        print(*line, sep=' ', file=output)
+
+
 
 f.close()
 output.close()
